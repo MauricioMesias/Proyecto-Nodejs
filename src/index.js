7eb6a2fdf01c5e30cc776eae5b1f06ea9f44fac2
Mauricio Mesias <mauricio.mesias.munoz@ciisa.cl>
2022-09-17 16:42:25 -0300

    puerto actualizado

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ app.use(indexRoutes)
 app.use(express.static(join(__dirname, 'public')))
 
 
-app.listen(3000)
-console.log('Server is listening on port', 3000)
+app.listen(process.env.PORT || 3000)
+console.log('Server is listening on port', process.env.PORT || 3000)
