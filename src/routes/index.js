3de22f2b3b87832b1ed18296957776bb3b3746fd
Mauricio Mesias <mauricio.mesias.munoz@ciisa.cl>
2022-09-17 16:32:08 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => res.render('index', {title: 'First Web with Node'}));
+
+router.get('/about', (req, res) => res.render('about', {title: 'About Me'}));
+
+router.get('/contact', (req, res) => res.render('contact', {title: 'Contact Page'}));
+
+export default router;
